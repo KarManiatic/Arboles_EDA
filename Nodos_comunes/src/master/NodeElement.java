c817b51b8425c8695cf9c566aacf47bab588f2c0
KarManiatic <carromnav@outlook.es>
2021-12-27 16:48:44 +0100

    Primer commit

---
+++ Nodos_comunes/src/master/NodeElement.java
@@ -0,0 +1,26 @@
+package master;
+
+class NodeElement <V,T> {
+	private V key;
+	private T value;
+	
+	public NodeElement(V k, T v) {
+		key = k;
+		value = v;
+	}
+	public V getKey() {
+		return key;
+	}
+	public void setKey(V k) {
+		key = k;
+	}
+	public T getValue() {
+		return value;
+	}
+	public void setValue(T v) {
+		value = v;
+	}
+	public boolean equals(Object n) {
+		return key.equals(((NodeElement<T, V>) n).getKey());
+	}
+}
---
+++ Nodos_comunes/src/module-info.java
@@ -0,0 +1,2 @@
+module Nodos_comunes {
+}
